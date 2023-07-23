d511ca554174536bfbc6dce79d95c1659b559ab9
Sehne, Julian <julian.sehne@student.fhws.de>
2023-07-23 18:49:11 +0200

    Add smoothness

--- Assets/VRRig.cs
+++ Assets/VRRig.cs
@@ -25,6 +25,8 @@ public class VRRig : MonoBehaviour
 
     public Transform headConstraint;
     public Vector3 headBodyOffset;
+
+    public int turnSmoothness = 5;
     void Start()
     {
         headBodyOffset = transform.position - headConstraint.position;
@@ -34,7 +36,7 @@ public class VRRig : MonoBehaviour
     void Update()
     {
         transform.position = headConstraint.position + headBodyOffset;
-        transform.forward = Vector3.ProjectOnPlane(headConstraint.up, Vector3.up).normalized;
+        transform.forward = Vector3.Lerp(transform.forward, Vector3.ProjectOnPlane(headConstraint.up, Vector3.up).normalized, Time.deltaTime * turnSmoothness);
         head.Map();
         leftHand.Map();
         rightHand.Map();
