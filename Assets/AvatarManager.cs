75fce42d1489368b2ca8b00400123e3830f983da
Julian <solution.julian@gmail.com>
2023-09-09 13:56:25 +0200

    Import textMeshPro stuff

--- Assets/AvatarManager.cs
+++ Assets/AvatarManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System;
+using TMPro;
 
 public class AvatarManager : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class AvatarManager : MonoBehaviour
     //Change avatar
     public GameObject[] allObjects;
     public List<GameObject> menuObjects;
+    public List<TextMeshPro> textObjects;
     public GameObject nearestObject;
     public GameObject panel;
     public GameObject globalAvatar;
@@ -18,6 +20,15 @@ public class AvatarManager : MonoBehaviour
     void Start()
     {
         AddNearestObjectOnButtonPress();
+
+        foreach (Transform child in panel.transform)
+        {
+            //child is your child transform
+            Debug.Log(child.GetChild(0).name);
+            Debug.Log(child.GetComponentInChildren<TextMeshPro>());
+            Debug.Log(child.GetComponentInChildren<TextMeshPro>().text);
+            textObjects.Add(child.GetComponentInChildren<TextMeshPro>());
+        }
     }
 
     // Update is called once per frame
